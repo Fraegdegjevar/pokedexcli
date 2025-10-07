f6da3bfd0417548f5c3ea1274a6344ecd8361660
Fraegdegjevar <willhcode@gmail.com>
2025-10-07 23:26:32 +0100

    Added http.go in pokeapi. Move request build and receive logiv from map and mapb to GetLocationAreas in pokeapi. Now returns LocationAreaResponse, error.

--- internal/command/commandMapb.go
+++ internal/command/commandMapb.go
@@ -1,9 +1,7 @@
 package command
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"net/url"
 
 	"github.com/Fraegdegjevar/pokedexcli/internal/pokeapi"
@@ -19,45 +17,23 @@ func commandMapb(conf *pokeapi.Config) error {
 		return nil
 	}
 
-	//Build request
-	req, err := http.NewRequest("GET", fullURL.String(), nil)
-	if err != nil {
-		fmt.Println("Error generating NewRequest")
-		return err
-	}
+	locationAreas, err := pokeapi.GetLocationAreas(fullURL, conf)
 
-	//initialise HTTP client
-	client := &http.Client{}
-	//Do request
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error fetching map info from pokeapi")
-		return err
-	}
-
-	//Decode response
-	var data pokeapi.LocationAreaResponse
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		fmt.Println("Error decoding response body")
-		return err
-	}
 	//Update config with the new next and previous pages from response
-	conf.Next, err = url.Parse(data.Next)
+	conf.Next, err = url.Parse(locationAreas.Next)
 	if err != nil {
 		fmt.Println("Error parsing response Next URL field as a url.URL")
 		return err
 	}
-	conf.Previous, err = url.Parse(data.Previous)
+	conf.Previous, err = url.Parse(locationAreas.Previous)
 	if err != nil {
 		fmt.Println("Error parsing response Previous URL field as a url.URL")
 		return err
 	}
 
 	//Print the location area names
-	for _, locarea := range data.Results {
+	for _, locarea := range locationAreas.Results {
 		fmt.Println(locarea.Name)
 	}
-
 	return nil
 }
---
+++ internal/pokeapi/http.go
@@ -0,0 +1,35 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func GetLocationAreas(fullURL *url.URL, conf *Config) (LocationAreaResponse, error) {
+	//Build request
+	req, err := http.NewRequest("GET", fullURL.String(), nil)
+	if err != nil {
+		fmt.Println("Error generating NewRequest")
+		return LocationAreaResponse{}, err
+	}
+
+	//initialise HTTP client
+	client := &http.Client{}
+	//Do request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error fetching map info from pokeapi")
+		return LocationAreaResponse{}, err
+	}
+
+	//Decode response
+	var data LocationAreaResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("Error decoding response body")
+		return LocationAreaResponse{}, err
+	}
+	return data, nil
+}
