c77540e33309c89a9b8b2250d7b5dc8cf0fc87bf
Fraegdegjevar <willhcode@gmail.com>
2025-10-07 23:29:45 +0100

    Modify commandMap to use GetLocationAreas function

--- internal/command/commandMap.go
+++ internal/command/commandMap.go
@@ -1,9 +1,7 @@
 package command
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"net/url"
 
 	"github.com/Fraegdegjevar/pokedexcli/internal/pokeapi"
@@ -22,46 +20,26 @@ func commandMap(conf *pokeapi.Config) error {
 		fullURL = conf.Next
 	}
 
-	req, err := http.NewRequest("GET", fullURL.String(), nil)
+	locationAreas, err := pokeapi.GetLocationAreas(fullURL, conf)
 	if err != nil {
-		fmt.Println("Error generating NewRequest")
-		return err
-	}
-
-	//initialise http client
-	client := &http.Client{}
-	//initialise response struct
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error fetching map info from pokeapi")
-		return err
-	}
-	defer resp.Body.Close()
-
-	//decode response
-	var data pokeapi.LocationAreaResponse
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		fmt.Println("Error decoding response body")
 		return err
 	}
 
 	//Store the next and previous urls in the API response in conf
-	conf.Next, err = url.Parse(data.Next)
+	conf.Next, err = url.Parse(locationAreas.Next)
 	if err != nil {
 		fmt.Println("Error parsing response next URL field as a url.URL")
 		return err
 	}
-	conf.Previous, err = url.Parse(data.Previous)
+	conf.Previous, err = url.Parse(locationAreas.Previous)
 	if err != nil {
 		fmt.Println("Error parsing response previous URL field as a url.URL")
 		return err
 	}
 
 	//Now fetch the names of the location-areas within results slice
-	for _, locarea := range data.Results {
+	for _, locarea := range locationAreas.Results {
 		fmt.Println(locarea.Name)
 	}
-
 	return nil
 }
